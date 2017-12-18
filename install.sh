5fc1676b8ffa4b02d8fba6c5c9a9f4af45b66202
Sam Adams <samharad@umich.edu>
2017-12-18 16:35:01 -0500

    added gitignore_global

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+##
+# Note that the symlink commands delete any existing files of the same name.
+##
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Vim
+ln -sf ${BASEDIR}/.vimrc ~/.vimrc
+# Because the second argument is a directory, ln will make a symlink
+# to .vim and place this link within the ~ directory
+ln -sFf ${BASEDIR}/.vim ~
+
+# Zsh
+ln -sf ${BASEDIR}/.zshrc ~/.zshrc
+
+# Git
+ln -sf ${BASEDIR}/.gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 
 source .macos
