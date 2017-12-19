42e6d453bbf0821fc1f4e43c54fb46929888e0a0
Sam Adams <samharad@umich.edu>
2017-12-18 19:50:02 -0500

    updated install

--- install.sh
+++ install.sh
@@ -6,17 +6,20 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Vim
-ln -sf ${BASEDIR}/.vimrc ~/.vimrc
-# Because the second argument is a directory, ln will make a symlink
-# to .vim and place this link within the ~ directory
-ln -sFf ${BASEDIR}/.vim ~
-
 # Zsh
 ln -sf ${BASEDIR}/.zshrc ~/.zshrc
 
 # Git
 ln -sf ${BASEDIR}/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+# Vim
+ln -sf ${BASEDIR}/.vimrc ~/.vimrc
+# Because the second argument is a directory, ln will make a symlink
+# to .vim and place this link within the ~ directory
+ln -sFf ${BASEDIR}/.vim ~
+cd ${BASEDIR}/.vim
+git submodule update --init
+
 
 source .macos
